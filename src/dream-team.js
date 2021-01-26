bf9c1162a13effd810fe83e3e1d7952efa76a6da
igotbitches <check.point9917@yandex.ru>
2021-01-26 18:02:25 +0300

    feal: create and complite function at dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (members !== null && members !== undefined) {
+    let array = new Array();
+    for (let i = 0; i < members.length; i++)
+      if (typeof members[i] === "string") {
+        let j = 0;
+        while (true) {
+          if (members[i][j] === " ") {
+            j++;
+          } else {
+            array.push(members[i][j]);
+            break;
+          }
+        }
+      }
+    return array.join("").toUpperCase().split("").sort().join("");
+  }
+  return false;
 };
