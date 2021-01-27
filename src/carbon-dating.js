1cf7d8e87c996867a37d5c2fb9101c74c5f82226
igotbitches <check.point9917@yandex.ru>
2021-01-27 20:48:25 +0300

    feat: create dunction for carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const T_PERIOD = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (sampleActivity !== "string") {
+    return false;
+  }
+
+  sampleActivity = Number(sampleActivity);
+
+  if (sampleActivity > MODERN_ACTIVITY || sampleActivity <= 0) {
+    return false;
+  }
+
+  k = T_PERIOD / HALF_LIFE_PERIOD;
+  date = Math.log(MODERN_ACTIVITY / sampleActivity) / k;
+  return Math.ceil(date);
 };
