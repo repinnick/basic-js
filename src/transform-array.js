4e9ec1cb6ed6d6a60e5c91eb752adde50c16eec4
igotbitches <check.point9917@yandex.ru>
2021-01-27 21:54:22 +0300

    feat: create function for hanoi-tower.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (typeof arr !== "object") throw Error;
+  arr2 = new Array();
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i + 1] === "--discard-prev" || arr[i] === "--discard-prev") {
+    } else if (arr[i] === "--discard-next") i++;
+    else if (arr[i] === "--double-next") {
+      if (arr[i + 1] !== undefined) arr2.push(arr[i + 1]);
+    } else if (arr[i] == "--double-prev") {
+      if (arr[i - 2] == "--discard-next") {
+      } else if (arr[i - 1] !== undefined) arr2.push(arr[i - 1]);
+    } else arr2.push(arr[i]);
+  }
+  return arr2;
 };
