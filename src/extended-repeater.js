ae0855f51d25b0852c2bd58aa4fc2d5b5336599e
igotbitches <check.point9917@yandex.ru>
2021-01-31 21:01:08 +0300

    working function (update 2)

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -24,16 +24,4 @@ module.exports = function repeater(str, options) {
   }
 
   return combineString;
-
-  // console.log(combineString);
 };
-
-// repeater("STRING", {
-//   repeatTimes: 3,
-//   separator: "**",
-//   addition: "PLUS",
-//   additionRepeatTimes: 3,
-//   additionSeparator: "00",
-// });
-
-// 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
