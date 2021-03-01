5f3cbc41c21c1d77086e5770097098289550fe56
igotbitches <check.point9917@yandex.ru>
2021-03-01 16:52:23 +0300

    feat: create depth func

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError("Not implemented");
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let maxStepToDive = 1;
+
+    arr.forEach((element) => {
+      let counter = 0;
+      if (Array.isArray(element)) {
+        counter += 1 + this.calculateDepth(element);
+      }
+      if (counter > maxStepToDive) {
+        maxStepToDive = counter;
+      }
+    });
+
+    return maxStepToDive;
   }
 };
--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,2 +1,9 @@
-ci = (pi + kj) % 26;
-// pi = (ci % 26) - kj;
+
+// function callToCalculate(array) {
+//   let counter = 1;
+//   for (let i = 0; i < array.length; i++) {
+//     if (Array.isArray(array)) {
+//       counter++;
+//     }
+//   }
+// }
